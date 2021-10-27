fc971f268f4fa0ad027442836f7c0519a17598ee
aaronmell <aaronamell@gmail.com>
2021-10-26 21:09:02 -0500

    Rudimentary play runner
    Added quarter increments, end of game during regulation play.

--- controller/simulate_game.go
+++ controller/simulate_game.go
@@ -2,6 +2,8 @@ package controller
 
 import (
 	"college-football-sim/utils"
+	"math/rand"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog/log"
@@ -15,14 +17,48 @@ type Team struct {
 }
 
 type SimulateGameRequest struct {
-	Team1 Team `json:"Team1" binding:"required"`
-	Team2 Team `json:"Team2" binding:"required"`
+	HomeTeam Team `json:"HomeTeam" binding:"required"`
+	AwayTeam Team `json:"AwayTeam" binding:"required"`
 }
 
 type SimulateGameResponse struct {
-	Winner Team `json:"Team1" binding:"required"`
-	Loser  Team `json:"Team2" binding:"required"`
+	Winner Team `json:"Winner" binding:"required"`
+	Loser  Team `json:"Loser" binding:"required"`
 }
+
+type GameStatus struct {
+	HomeTeam           Team
+	AwayTeam           Team
+	GameOver           bool
+	Quarter            Quarter
+	GameClockInSeconds int
+	HomeTeamScore      int
+	AwayTeamScore      int
+}
+
+type Quarter int
+
+const (
+	First Quarter = iota
+	Second
+	Third
+	Fourth
+	Overtime1
+	Overtime2
+	Overtime3
+	Overtime4
+	Overtime5
+	Overtime6
+	Overtime7
+	Overtime8
+	Overtime9
+	Overtime10
+	Overtime11
+	Overtime12
+	Overtime13
+	Overtime14
+	Overtime15
+)
 
 func (sgc SimulateGameController) SimulateGame(c *gin.Context) {
 	reqBody := SimulateGameRequest{}
@@ -33,14 +69,52 @@ func (sgc SimulateGameController) SimulateGame(c *gin.Context) {
 	}
 
 	response := SimulateGameResponse{}
+	gameStatus := initializeGameStatus(reqBody)
 
-	if reqBody.Team1.Overall > reqBody.Team2.Overall {
-		response.Winner = reqBody.Team1
-		response.Loser = reqBody.Team2
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	for ok := true; ok; ok = gameStatus.GameOver {
+		runPlay(&gameStatus, r1)
+	}
+
+	if reqBody.HomeTeam.Overall > reqBody.AwayTeam.Overall {
+		response.Winner = reqBody.HomeTeam
+		response.Loser = reqBody.AwayTeam
 	} else {
-		response.Winner = reqBody.Team2
-		response.Loser = reqBody.Team1
+		response.Winner = reqBody.AwayTeam
+		response.Loser = reqBody.HomeTeam
 	}
 
 	c.JSON(utils.Success.Status, response)
+}
+
+func runPlay(gs *GameStatus, r *rand.Rand) {
+	if gs.Quarter <= Fourth {
+		runRegulationPlay(gs, r)
+		return
+	}
+
+}
+
+func runRegulationPlay(gs *GameStatus, r *rand.Rand) {
+	if gs.GameClockInSeconds == 0 {
+		if gs.Quarter == Fourth {
+			if gs.HomeTeamScore != gs.AwayTeamScore {
+				gs.GameOver = true
+				return
+			}
+		}
+		gs.Quarter++
+		return
+	}
+}
+
+func initializeGameStatus(reqBody SimulateGameRequest) GameStatus {
+	return GameStatus{
+		HomeTeam:           reqBody.HomeTeam,
+		AwayTeam:           reqBody.AwayTeam,
+		GameOver:           false,
+		Quarter:            First,
+		GameClockInSeconds: 900,
+	}
 }
--- controller/simulate_game_test.go
+++ controller/simulate_game_test.go
@@ -4,13 +4,16 @@ import (
 	"bytes"
 	"encoding/json"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"college-football-sim/utils"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestSimulateGame(t *testing.T) {
@@ -20,11 +23,11 @@ func TestSimulateGame(t *testing.T) {
 	r.POST("/", sgc.SimulateGame)
 
 	simulateGameRequest := &SimulateGameRequest{
-		Team1: Team{
+		HomeTeam: Team{
 			Name:    "Team1",
 			Overall: 51,
 		},
-		Team2: Team{
+		AwayTeam: Team{
 			Name:    "Team2",
 			Overall: 52,
 		},
@@ -52,4 +55,149 @@ func TestSimulateGame(t *testing.T) {
 
 		return statusOK && response.Winner.Name == "Team2"
 	})
+}
+
+func TestRunPlay(t *testing.T) {
+	tests := map[string]struct {
+		input GameStatus
+		want  GameStatus
+	}{
+		"GameOverNotTied": {
+			input: GameStatus{
+				GameClockInSeconds: 0,
+				GameOver:           false,
+				Quarter:            Fourth,
+				HomeTeamScore:      7,
+				AwayTeamScore:      0,
+			},
+			want: GameStatus{
+				GameClockInSeconds: 0,
+				GameOver:           true,
+				Quarter:            Fourth,
+				HomeTeamScore:      7,
+				AwayTeamScore:      0,
+			},
+		},
+		"NextQuarterWhenTime0": {
+			input: GameStatus{
+				GameClockInSeconds: 0,
+				GameOver:           false,
+				Quarter:            Third,
+				HomeTeamScore:      7,
+				AwayTeamScore:      0,
+			},
+			want: GameStatus{
+				GameClockInSeconds: 0,
+				GameOver:           false,
+				Quarter:            Fourth,
+				HomeTeamScore:      7,
+				AwayTeamScore:      0,
+			},
+		},
+		"OvertimeWhenScoreTied": {
+			input: GameStatus{
+				GameClockInSeconds: 0,
+				GameOver:           false,
+				Quarter:            Fourth,
+				HomeTeamScore:      7,
+				AwayTeamScore:      7,
+			},
+			want: GameStatus{
+				GameClockInSeconds: 0,
+				GameOver:           false,
+				Quarter:            Overtime1,
+				HomeTeamScore:      7,
+				AwayTeamScore:      7,
+			},
+		},
+	}
+
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			tc.input.HomeTeam = Team{
+				Name: "Team1",
+			}
+			tc.input.AwayTeam = Team{
+				Name: "Team2",
+			}
+
+			tc.want.HomeTeam = Team{
+				Name: "Team1",
+			}
+			tc.want.AwayTeam = Team{
+				Name: "Team2",
+			}
+
+			runPlay(&tc.input, r1)
+			diff := cmp.Diff(tc.want, tc.input)
+
+			if diff != "" {
+				t.Fatalf(diff)
+			}
+
+		})
+	}
+
+	// t.Run("Returns Gameover when time is 0 and quarter is fourth and score is not tied", func(t *testing.T) {
+	// 	gameStatus := &
+
+	// 	s1 := rand.NewSource(1)
+	// 	r1 := rand.New(s1)
+	// 	runPlay(gameStatus, r1)
+
+	// 	if gameStatus.GameOver != true {
+	// 		t.Error("Expected Gameover to be true, recieved false")
+	// 	}
+
+	// 	if gameStatus.Quarter != Fourth {
+	// 		t.Errorf("Expected quarter %d recieved %d", Fourth, gameStatus.Quarter)
+	// 	}
+	// })
+
+	// t.Run("Increments Quarter when time is 0, and quarter is less than the fourth or when quarter is 4 or greater and score is tied", func(t *testing.T) {
+	// 	cases := []struct {
+	// 		Name          string
+	// 		StartQuarter  Quarter
+	// 		EndQuarter    Quarter
+	// 		HomeTeamScore int
+	// 		AwayTeamScore int
+	// 	}{
+	// 		{First, Second},
+	// 		{Second, Third},
+	// 		{Third, Fourth},
+	// 		{Fourth, Overtime1},
+	// 	}
+
+	// 	for _, test := range cases {
+	// 		gameStatus := &GameStatus{
+	// 			GameClockInSeconds: 0,
+	// 			GameOver:           false,
+	// 			Quarter:            test.StartQuarter,
+	// 			HomeTeam: Team{
+	// 				Name: "Team1",
+	// 			},
+	// 			AwayTeam: Team{
+	// 				Name: "Team2",
+	// 			},
+	// 			HomeTeamScore: 0,
+	// 			AwayTeamScore: 0,
+	// 		}
+
+	// 		s1 := rand.NewSource(1)
+	// 		r1 := rand.New(s1)
+	// 		runPlay(gameStatus, r1)
+
+	// 		if gameStatus.GameOver == true {
+	// 			t.Errorf("Expected gameover to be false in quarter %d, was true", test)
+	// 		}
+
+	// 		if gameStatus.Quarter != test.EndQuarter {
+	// 			t.Errorf("Expected Quarter to be %d was %d", test.EndQuarter, test.StartQuarter)
+	// 		}
+
+	// 	}
+	// })
 }
